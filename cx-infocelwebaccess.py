6b3049371c1018210884a33499b7946510f67c38
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2025-08-13 12:07:39 +0200

    Update cx-infocelwebaccess.py

--- cx-infocelwebaccess.py
+++ cx-infocelwebaccess.py
@@ -13,5 +13,5 @@ st.set_page_config(
 
 st.image("https://i.imgur.com/NwOV7Ob.jpg")
 
-df_infocelaccessDB = pd.read_csv("BDAccesosInfocel.xlsx")
+df_infocelaccessDB = pd.read_csv("BD.csv")
 st.write(df_infocelaccessDB)
