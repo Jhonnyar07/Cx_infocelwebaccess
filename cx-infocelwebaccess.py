d396d228113e640dda355cb6fa49edbdaaa5b915
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2025-08-13 14:48:39 +0200

    Update cx-infocelwebaccess.py

--- cx-infocelwebaccess.py
+++ cx-infocelwebaccess.py
@@ -19,6 +19,7 @@ if "logueado" not in st.session_state:
     st.session_state.logueado = False
 
 if not st.session_state.logueado:
+    st.image("static/login.png")
     st.markdown("<h2 style='text-align: center;'>Login de Acceso</h2>", unsafe_allow_html=True)
     username = st.text_input("Usuario")
     password = st.text_input("Contraseña", type="password")
