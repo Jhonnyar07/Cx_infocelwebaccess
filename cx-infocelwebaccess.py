c3347aaf7886535789c63bbe639039efb081eda6
Jhonnyar07 <109990667+Jhonnyar07@users.noreply.github.com>
2025-08-13 11:05:41 +0200

    Update and rename streamlit_app.py to cx-infocelwebaccess.py

---
+++ cx-infocelwebaccess.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import math
+from PIL import Image
+
+#@st.cache
+
+#im = chart_with_upwards_trend
+st.set_page_config(
+    page_title="Buscador de accesos de clientes"
+)
+
+st.image("https://i.imgur.com/NwOV7Ob.jpg")
